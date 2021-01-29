87673f0eb6303e27dbdd6f5fd7d28b0ad64f313d
Marc Finzi <mfinzi@hmc.edu>
2021-01-29 14:54:22 -0500

    added data augmentation

--- experiments/lorentz_jax.py
+++ experiments/lorentz_jax.py
@@ -4,13 +4,12 @@ import jax.numpy as jnp
 import jax
 from emlp_jax.equivariant_subspaces import T,Scalar,Matrix,Vector
 from emlp_jax.groups import SO,O,Trivial,Lorentz,O13,SO13,SO13p
-from emlp_jax.mlp import EMLP,LieLinear,Standardize,EMLP2
+from emlp_jax.mlp import EMLP,LieLinear,Standardize
 from emlp_jax.model_trainer import RegressorPlus
 import itertools
 import numpy as np
 import torch
 from emlp_jax.datasets import Inertia,Fr,ParticleInteraction
-import objax
 import torch
 from torch.utils.data import DataLoader
 from oil.utils.utils import cosLr, islice, export,FixedNumpySeed,FixedPytorchSeed
@@ -22,11 +21,12 @@ from functools import partial
 import torch.nn as nn
 import logging
 import emlp_jax
+import objax
 #repmiddle = 100*T(0)+30*T(1)+10*T(2)+3*T(3)#+1*T(4)
 
 
-def makeTrainer(*,dataset=ParticleInteraction,network=EMLP,num_epochs=500,ndata=1000+1000,seed=2020,aug=False,
-                bs=500,lr=1e-3,device='cuda',split={'train':-1,'test':1000},
+def makeTrainer(*,dataset=ParticleInteraction,network=EMLP,num_epochs=200,ndata=1000+1000,seed=2021,aug=False,
+                bs=500,lr=3e-3,device='cuda',split={'train':-1,'test':1000},
                 net_config={'num_layers':3,'ch':384,'group':SO13p()},log_level='info',
                 trainer_config={'log_dir':None,'log_args':{'minPeriod':.02,'timeFrac':.25}},save=False):
     levels = {'critical': logging.CRITICAL,'error': logging.ERROR,
@@ -36,13 +36,15 @@ def makeTrainer(*,dataset=ParticleInteraction,network=EMLP,num_epochs=500,ndata=
     # Prep the datasets splits, model, and dataloaders
     with FixedNumpySeed(seed),FixedPytorchSeed(seed):
         datasets = split_dataset(dataset(ndata),splits=split)
-    model = Standardize(network(datasets['train'].rep_in,datasets['train'].rep_out,**net_config),datasets['train'].stats)
+    model = network(datasets['train'].rep_in,datasets['train'].rep_out,**net_config)
+    if aug: model = datasets['train'].default_aug(model)
+    model = Standardize(model,datasets['train'].stats)
     dataloaders = {k:LoaderTo(DataLoader(v,batch_size=bs,shuffle=(k=='train'),
                 num_workers=0,pin_memory=False)) for k,v in datasets.items()}
     dataloaders['Train'] = dataloaders['train']
     #equivariance_test(model,dataloaders['train'],net_config['group'])
     opt_constr = objax.optimizer.Adam
-    lr_sched = lambda e: lr*cosLr(num_epochs)(e)*min(1,.e/(num_epochs/10))
+    lr_sched = lambda e: lr*cosLr(num_epochs)(e)#*min(1,e/(num_epochs/10))
     return RegressorPlus(model,dataloaders,opt_constr,lr_sched,**trainer_config)
 
 if __name__ == "__main__":

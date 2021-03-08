89f26bef5a58a319ce9a1fe33ef272d914ab1c7c
Marc Finzi <mfinzi@hmc.edu>
2021-03-08 01:27:42 -0500

    init import for models

--- emlp/models/__init__.py
+++ emlp/models/__init__.py
@@ -1,5 +1,7 @@
 from emlp.models import mlp
 from emlp.models import batchnorm
+from emlp.models.mlp import *
+from emlp.models.batchnorm import *
 __all__ = []
 __all__ += mlp.__all__
 __all__ += batchnorm.__all__

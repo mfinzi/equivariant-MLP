96676793f5d10ea069d561c6cd9aa38ba4dbdd3b
Marc Finzi <mfinzi@hmc.edu>
2021-04-19 22:48:11 -0400

    aaaa!! dont panic. I broke imports quick fix

--- emlp/nn/__init__.py
+++ emlp/nn/__init__.py
@@ -1,2 +1,6 @@
-
-from .objax import *
+import importlib
+import pkgutil
+__all__ = []
+module = importlib.import_module('.'+'objax',package=__name__)
+globals().update({k: getattr(module, k) for k in module.__all__})
+__all__ += module.__all__

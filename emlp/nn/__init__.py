df0f56ff806454786f2a01e63259656707b6d9a3
Marc Finzi <mfinzi@hmc.edu>
2021-04-21 13:08:35 -0400

    Update __init__.py

--- emlp/nn/__init__.py
+++ emlp/nn/__init__.py
@@ -1,6 +1,6 @@
 import importlib
 import pkgutil
-__all__ = []
+__all__ = [] # expose objax implementation as base nn
 module = importlib.import_module('.'+'objax',package=__name__)
 globals().update({k: getattr(module, k) for k in module.__all__})
 __all__ += module.__all__
